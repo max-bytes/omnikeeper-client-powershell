a53854d232be24e7125f5eefd4e56cb766060814
generator <generator@max-bytes.com>
2022-04-06 08:23:05 +0000

    Update to version 15.0.0-rc14

--- Build.ps1
+++ Build.ps1
@@ -54,10 +54,10 @@ $Manifest = @{
     CompanyName = 'openapitools.org'
     Description = 'okclient - the PowerShell module for Landscape omnikeeper REST API'
 
-    ModuleVersion = '15.0.0-rc13'
+    ModuleVersion = '15.0.0-rc14'
 
     RootModule = 'okclient.psm1'
-    Guid = '{7E1FF3F1-2E98-4108-BF8E-DCF77BA6D789}' # Has to be static, otherwise each new build will be considered different module
+    Guid = '{B95CCDC2-0E71-4EED-8605-A21A0B7B0A22}' # Has to be static, otherwise each new build will be considered different module
 
     PowerShellVersion = '6.2'
 
